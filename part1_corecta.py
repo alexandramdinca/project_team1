481441da3efab234680c1551ff0efac6815ec893
bianca <biancapresa17@gmail.com>
2025-04-16 17:48:00 +0300

    add file

--- part1_corecta.py
+++ part1_corecta.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import DECIMAL, Column, DateTime, ForeignKey, Integer, Numeric, String, create_engine
+from sqlalchemy import DECIMAL, Column, DateTime, ForeignKey, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
@@ -12,8 +12,8 @@ Base = declarative_base()
 
 class Plants(Base):
     __tablename__ = 'Plants'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    name = Column(String, unique = True, nullable = False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
     location = Column(String)
     capacity = Column(Integer)
     plants_products = relationship("PlantsProducts", back_populates="plants")
@@ -21,10 +21,10 @@ class Plants(Base):
 
 class Products(Base):
     __tablename__ = 'Products'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    name = Column(String, unique = True, nullable = False)
-    description = Column(String, nullable = True)
-    category = Column(String, nullable = False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String, nullable=True)
+    category = Column(String, nullable=False)
     price = Column(DECIMAL)
     plants_products = relationship("PlantsProducts", back_populates="products")
     storage_products = relationship("StorageProducts", back_populates="products")
@@ -33,10 +33,10 @@ class Products(Base):
 
 class Materials(Base):
     __tablename__ = 'Materials'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    name = Column(String, unique = True, nullable = False)
-    description = Column(String, nullable = True)
-    unit = Column(String, nullable = True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String, nullable=True)
+    unit = Column(String, nullable=True)
     cost = Column(DECIMAL)
     plants_materials = relationship("PlantsMaterials", back_populates="materials")
     storage_materials = relationship("StorageMaterials", back_populates="materials")
@@ -44,72 +44,70 @@ class Materials(Base):
 
 class Orders(Base):
     __tablename__ = 'Orders'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    order_date = Column(DateTime, nullable = False)
-    customer_name = Column(String, nullable = False)
-    status = Column(String, nullable = False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_date = Column(DateTime, nullable=False)
+    customer_name = Column(String, nullable=False)
+    status = Column(String, nullable=False)
     orders_products = relationship("OrdersProducts", back_populates="orders")
 
 class PlantsProducts(Base):
     __tablename__ = 'PlantsProducts'
-    id = Column(Integer, primary_key = True, autoincrement = True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     plant_id = Column(Integer, ForeignKey('Plants.id'))
     product_id = Column(Integer, ForeignKey('Products.id'))
     quantity = Column(DECIMAL)
-    plants = relationship("Plants", back_populates="plants")
-    products = relationship("Products", back_populates="products")
+    plants = relationship("Plants", back_populates="plants_products")
+    products = relationship("Products", back_populates="plants_products")
 
 class ProductsMaterials(Base):
     __tablename__ = 'ProductsMaterials'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    product_id = Column(Integer,  ForeignKey('Products.id'))
-    material_id = Column(Integer,  ForeignKey('Materials.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey('Products.id'))
+    material_id = Column(Integer, ForeignKey('Materials.id'))
     quantity = Column(DECIMAL)
-    products = relationship("Products", back_populates="products")
-    materials = relationship("Materials", back_populates="materials")
-
+    products = relationship("Products", back_populates="products_materials")
+    materials = relationship("Materials", back_populates="products_materials")
 
 class PlantsMaterials(Base):
-    __tablename__ = 'PlantsMaterias'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    plant_id = Column(Integer,  ForeignKey('Plants.id'))
-    material_id = Column(Integer,  ForeignKey('Materials.id'))
+    __tablename__ = 'PlantsMaterials'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    plant_id = Column(Integer, ForeignKey('Plants.id'))
+    material_id = Column(Integer, ForeignKey('Materials.id'))
     quantity = Column(DECIMAL)
-    materials = relationship("Materials", back_populates="materials")
-    plants = relationship("Plants", back_populates="plants")
+    materials = relationship("Materials", back_populates="plants_materials")
+    plants = relationship("Plants", back_populates="plants_materials")
 
 class OrdersProducts(Base):
     __tablename__ = 'OrdersProducts'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    order_id = Column(Integer,  ForeignKey('Orders.id'))
-    product_id = Column(Integer,  ForeignKey('Products.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_id = Column(Integer, ForeignKey('Orders.id'))
+    product_id = Column(Integer, ForeignKey('Products.id'))
     quantity = Column(Integer)
-    products = relationship("Products", back_populates="products")
-    orders = relationship("Orders", back_populates="orders")
+    products = relationship("Products", back_populates="orders_products")
+    orders = relationship("Orders", back_populates="orders_products")
 
 class StorageProducts(Base):
     __tablename__ = 'StorageProducts'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    product_id = Column(Integer,  ForeignKey('Products.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey('Products.id'))
     quantity = Column(Integer)
-    products = relationship("Products", back_populates="products")
-    storage = relationship("Storage", back_populates="storage")
+    products = relationship("Products", back_populates="storage_products")
 
 class StorageMaterials(Base):
     __tablename__ = 'StorageMaterials'
-    id = Column(Integer, primary_key = True, autoincrement = True)
-    material_id = Column(Integer,  ForeignKey('Materials.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    material_id = Column(Integer, ForeignKey('Materials.id'))
     quantity = Column(Integer)
-    storage = relationship("Storage", back_populates="storage")
-    materials =  relationship("Materials", back_populates="materials")
+    materials = relationship("Materials", back_populates="storage_materials")
 
+# 🔧 Crearea bazei de date
 try:
-    with engine.connect() as connection:
-        print("Database connected successfully!")
+    Base.metadata.create_all(engine)
+    print("Tabelele au fost create cu succes!")
 except Exception as e:
-    print(f"Error connecting to database: {e}")
+    print(f"Eroare la crearea tabelelor: {e}")
 
-
+# ✅ Popularea bazei de date
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -204,5 +202,4 @@ orders_products = [
 session.add_all(orders_products)
 
 session.commit()
-
 session.close()
